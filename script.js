0c8590374454111b12495a9ca3c9bd7f4ebb309e
gray <89231821+deargraydev@users.noreply.github.com>
2021-11-25 14:32:49 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, world!");
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
